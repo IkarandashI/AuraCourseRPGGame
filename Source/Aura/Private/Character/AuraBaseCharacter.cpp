90d68ab8f96157121c765da656c26b4aaab13f60
IkarandashI <v.vitaminkin@gmail.com>
2024-04-09 17:56:04 +0300

    Impact Effects

--- Source/Aura/Private/Character/AuraBaseCharacter.cpp
+++ Source/Aura/Private/Character/AuraBaseCharacter.cpp
@@ -99,6 +99,11 @@ TArray<FTaggedMontage> AAuraBaseCharacter::GetAttackMontages_Implementation()
 	return AttackMontages;
 }
 
+UNiagaraSystem* AAuraBaseCharacter::GetBloodEffect_Implementation()
+{
+	return BloodEffect;
+}
+
 void AAuraBaseCharacter::InitAbilityActorInfo()
 {
 }
